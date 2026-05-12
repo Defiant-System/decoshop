c15d0b563779501b091285a99cf90df265e66bf3
Hakan Bilgin <hbi99@hotmail.com>
2026-05-12 14:20:04 +0200

    ui dom

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -6,31 +6,31 @@ let Test = {
 		// setTimeout(() => APP.els.content.find(`.sample:nth(0)`).trigger("click"), 100);
 
 		/* 
-		 * dlgCameraRaw
-		 * dlgLensCorrection
-		 * dlgFilterGallery
-		 * dlgLiquify
-		 * 
-		 * dlgNormalMap
-		 * dlgTextureDilation
-		 * 
-		 * dlgBoxBlur
-		 * dlgLensBlur
-		 * dlgMotionBlur
-		 * dlgRadialBlur
-		 * dlgSurfaceBlur
-		 * 
-		 * dlgBrightnessContrast
-		 * dlgStroke
-		 * dlgGaussianBlur
-		 * dlgThreshold
-		 * dlgCrystallize
-		 * dlgPointillize
-		 * dlgMosaic
-		 * dlgPixelator
-		 * dlgColors
+		 * dlgExportAs	dlgPrint	dlgExportLayers	dlgExportColorLookUp	dlgFileInfo
+		 * dlgFill	dlgStroke	dlgPresetManager	dlgPreferences
+		 * dlgCanvasSize	dlgImageSize	dlgTrim	dlgApplyImage	dlgVariables
+		 * dlgBrightnessContrast	dlgLevels	dlgCurves
+		   dlgExposure 	dlgVibrance 	dlgHueSaturation 	dlgColorBalance 	dlgBlackWhite 	dlgPhotoFilter
+		   dlgChannelMixer 	dlgColorLookup 	dlgThreshold 	dlgGradientMap 	dlgSelectiveColor 	dlgShadowHighlights
+		   dlgMatchColor	dlgReplaceColor	
+		 * dlgSelectColorRange	dlgSelectMagicCut	dlgSelectRefineEdge	dlgSelectModifyBorder
+		 * dlgSelectModifySmooth	dlgSelectModifyExpand	dlgSelectModifyContract	dlgSelectModifyFeather
+
+		 * dlgFilterGallery	dlgLensCorrection	dlgCameraRaw	dlgLiquify	dlgPixelator
+		 * dlgNormalMap	dlgTextureDilation
+
+		 * dlgBoxBlur	dlgGaussianBlur	dlgLensBlur	dlgMotionBlur	dlgRadialBlur	dlgSurfaceBlur
+		 * dlgDisplace	dlgKaleidoscope	dlgPinch	dlgPolarCoordinates	dlgRipple	dlgShear	dlgSpherize	dlgTwirl	dlgWave	dlgZigZag
+		 * dlgAddNoise	dlgDustScratches	dlgMedian	dlgReduceNoise
+		 * dlgColorHalftone	dlgCrystallize	dlgMezzoint	dlgMosaic	dlgPointillize
+		 * dlgFlame	dlgFibers	dlgLensFlare
+		 * dlgSmartSharpen	dlgUnshartMask
+
+		 * dlgDiffuse	dlgEmboss	dlgOilPaint	dlgTraceContour	dlgWind
+		 * dlgHighPass	dlgHsbHsl	dlgMaximum	dlgMinimum	dlgOffset	dlgRepeat	dlgColorToAlpha	dlgDither	dlgParticles
+		 * dlgAddGuides
 		 */
-		// setTimeout(() => APP.dispatch({ type: "open-dialog", arg: "dlgFilterGallery" }), 800);
+		setTimeout(() => APP.dispatch({ type: "open-dialog", arg: "dlgPreferences" }), 800);
 
 	}
 };
