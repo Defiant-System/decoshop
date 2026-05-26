d90027262fa08ac87729ef57c62daa42ba065d1e
Hakan Bilgin <hbi99@hotmail.com>
2026-05-26 11:53:14 +0200

    more dialog ui

--- src/js/modules/dialogs.js
+++ src/js/modules/dialogs.js
@@ -1979,6 +1979,29 @@ const Dialogs = {
 				break;
 		}
 	},
+	dlgChannelMixer(event) {
+		let APP = decoshop,
+			Self = Dialogs,
+			layers,
+			pixels,
+			copy,
+			el;
+		switch (event.type) {
+			case "set-cyan-red-value":
+				event.target.html(event.value)
+				/* falls through */
+			case "apply-filter-data":
+				return;
+			// standard dialog events
+			case "dlg-open":
+			case "dlg-ok":
+			case "dlg-reset":
+			case "dlg-preview":
+			case "dlg-close":
+				UI.doDialog({ ...event, type: `${event.type}-common`, name: "dlgChannelMixer" });
+				break;
+		}
+	},
 	dlgPixelator(event) {
 		let APP = decoshop,
 			Self = Dialogs,
