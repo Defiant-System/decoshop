2429f5992279113f7e7adb285a7fcbea852ce436
Hakan Bilgin <hbi99@hotmail.com>
2026-05-27 13:10:34 +0200

    dialog ui

--- src/js/modules/dialogs.js
+++ src/js/modules/dialogs.js
@@ -2313,6 +2313,27 @@ const Dialogs = {
 				break;
 		}
 	},
+	dlgFileInfo(event) {
+		let APP = decoshop,
+			Self = Dialogs,
+			el;
+		// console.log(event);
+		switch (event.type) {
+			case "set-cyan-red-value":
+				event.target.html(event.value)
+				/* falls through */
+			case "apply-filter-data":
+				return;
+			// standard dialog events
+			case "dlg-open":
+			case "dlg-ok":
+			case "dlg-reset":
+			case "dlg-preview":
+			case "dlg-close":
+				UI.doDialog({ ...event, type: `${event.type}-common`, name: "dlgFileInfo" });
+				break;
+		}
+	},
 	dlgPixelator(event) {
 		let APP = decoshop,
 			Self = Dialogs,
