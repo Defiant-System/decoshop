4b1e3e7b1a1984c6169b7c86fe020118d48467e9
Hakan Bilgin <hbi99@hotmail.com>
2026-07-11 21:38:47 +0200

    more filter

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -82,7 +82,7 @@ let Test = {
 		/* 
 		 * 
 		 */
-		setTimeout(() => APP.dispatch({ type: "open-dialog", arg: "dlgMaximum" }), 1600);
+		setTimeout(() => APP.dispatch({ type: "open-dialog", arg: "dlgTraceContour" }), 1600);
 		// setTimeout(() => APP.els.content.find(`.active-filters .icon-arrow`).get(0).trigger("click"), 1000);
 		// setTimeout(() => APP.els.content.find(`.active-filters .filter`).get(0).trigger("click"), 1400);
 
