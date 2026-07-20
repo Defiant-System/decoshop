2cc10744977845666bc32b7a3e8dd2414b61ae3b
Hakan Bilgin <hbi99@hotmail.com>
2026-07-20 10:32:37 +0200

    pipette / ruler tool

--- src/js/tools/pipette.js
+++ src/js/tools/pipette.js
@@ -11,22 +11,58 @@
 			el;
 		// console.log(event);
 		switch (event.type) {
+			// native events - proxy
+			case "mousedown":
+				switch (Self.option) {
+					case "pipette": return Self.doPipette(event);
+					case "ruler": return Self.doRuler(event);
+				}
+				break;
+
+			// custom events
+			case "select-option":
+				Self.option = event.arg || "pipette";
+				// reset cursor
+				APP.els.content.toggleClass(`cursor-pipette-1`, Self.option !== "pipette");
+				break;
+			case "enable":
+				// fast references
+				Self.els = {
+					wrapper: APP.els.content.find(`.cvs-wrapper`),
+					helpers: APP.els.content.find(`.cvs-helpers`),
+					sampler: APP.els.content.find(`.cvs-helpers .color-sampler`),
+				};
+				// set cursor
+				APP.els.content.addClass(`cursor-pipette-1`);
+				// bind event handlers
+				APP.els.cvs.on("mousedown", Self.dispatch);
+				break;
+			case "disable":
+				// set cursor
+				APP.els.content.removeClass(`cursor-pipette-1`);
+				// unbind event handlers
+				APP.els.cvs.off("mousedown", Self.dispatch);
+				break;
+		}
+	},
+	doPipette(event) {
+		let APP = decoshop,
+			Self = APP.tools.pipette,
+			Doc = APP.file.doc,
+			el;
+		switch (event.type) {
 			// native events
 			case "mousedown":
 				Self.rect = Self.els.helpers[0].getBoundingClientRect();
 				let { clientX, clientY, offsetX, offsetY } = event;
-				Self.dispatch({ type: "mousemove", clientX, clientY, offsetX, offsetY });
+				Self.doPipette({ type: "mousemove", clientX, clientY, offsetX, offsetY });
 				Self.els.sampler.addClass("show");
-				break;
-			case "mouseup":
-				Self.els.sampler.removeClass("show");
-				delete Self.rect;
+				
+				// bind event handlers
+				UI.doc.on("mousemove mouseup", Self.doPipette);
 				break;
 			case "mousemove":
-				if (!Self.rect) return;
-
-				let Doc = APP.file.doc,
-					docPoint = Doc.u.Zx(event.offsetX, event.offsetY),
+				let docPoint = Doc.u.Zx(event.offsetX, event.offsetY),
 					pixelCoord = new Point2D(Math.floor(docPoint.x), Math.floor(docPoint.y)),
 					top = (event.clientY - Self.rect.top) * DPR,
 					left = (event.clientX - Self.rect.left) * DPR,
@@ -48,28 +84,40 @@
 				// move tip
 				Self.els.sampler.css({ top, left, "--over": over, "--curr": curr });
 				break;
-
-			// custom events
-			case "select-option":
-				Self.option = event.arg || "pipette";
-				break;
-			case "enable":
-				// fast references
-				Self.els = {
-					wrapper: APP.els.content.find(`.cvs-wrapper`),
-					helpers: APP.els.content.find(`.cvs-helpers`),
-					sampler: APP.els.content.find(`.cvs-helpers .color-sampler`),
-				};
-				// set cursor
-				APP.els.content.addClass(`cursor-pipette-1`);
-				// bind event handlers
-				APP.els.cvs.on("mousedown mousemove mouseup", Self.dispatch);
-				break;
-			case "disable":
-				// set cursor
-				APP.els.content.removeClass(`cursor-pipette-1`);
+			case "mouseup":
+				Self.els.sampler.removeClass("show");
+				delete Self.rect;
 				// unbind event handlers
-				APP.els.cvs.off("mousedown mousemove mouseup", Self.dispatch);
+				UI.doc.off("mousemove mouseup", Self.doPipette);
+				break;
+		}
+	},
+	doRuler(event) {
+		let APP = decoshop,
+			Self = APP.tools.pipette,
+			Drag = Self.drag,
+			Doc = APP.file.doc,
+			el;
+		switch (event.type) {
+			case "mousedown":
+				// ruler
+				let p1 = Doc.u.Zx(event.offsetX, event.offsetY),
+					coord = new Point2D(Math.floor(p1.x), Math.floor(p1.y));
+				
+				Self.drag = { coord };
+				
+				// bind event handlers
+				UI.doc.on("mousemove mouseup", Self.doRuler);
+				break;
+			case "mousemove":
+				break;
+			case "mouseup":
+				let p2 = Doc.u.Zx(event.offsetX, event.offsetY),
+					coord2 = new Point2D(Math.floor(p2.x), Math.floor(p2.y));
+				
+				console.log(Drag.coord, coord2);
+				// bind event handlers
+				UI.doc.on("mousemove mouseup", Self.doRuler);
 				break;
 		}
 	}
